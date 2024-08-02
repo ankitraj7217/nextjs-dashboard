b9e3a9f23c100eabbcbfb7666c4cc1a659a13e2c
Ankit Raj <ankitraj7217@gmail.com>
2024-08-02 23:01:35 +0000

    Add relevant code for data fetching and mutation.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      serverActions: {
+        // Specify the allowed origins
+        allowedOrigins: ['my-forwarded-host.com', 'studious-xylophone-4gwxpq6r7543qr9-3000.app.github.dev', 'localhost:3000'],
+      },
+    },
+  };
+  
+  export default nextConfig;
+  
